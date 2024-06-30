ec1a1e6af211cf433a56beb1b1c73e6bfa6b3d2c
menarub1 <menarub1@students.zhaw.ch>
2024-06-30 14:00:55 +0200

    added example to core test

--- tuma/test/tuma/core_test.clj
+++ tuma/test/tuma/core_test.clj
@@ -3,6 +3,23 @@
             [tuma.turingmachine :as tm]
             [tuma.transition :as trans]))
 
+(deftest binary-add-by-one
+  (testing "binary add by one turingmachine")
+  (let [t1 (trans/new-transition :q1 "1" :q1 "1" :right)
+        t2 (trans/new-transition :q1 "0" :q1 "0" :right)
+        t3 (trans/new-transition :q1 "_" :q3 "_" :left)
+        t4 (trans/new-transition :q3 "1" :q3 "0" :left)
+        t5 (trans/new-transition :q3 "0" :q2 "1" :left)
+        transitions [t1 t2 t3 t4 t5]
+        states [:q1 :q2 :q3]
+        input-symbols ["0" "1"]
+        tape-symbols ["0" "1" "_"]
+        accepted-states [:q2]
+        tm (tm/new-turingmachine states input-symbols tape-symbols transitions :q1 "_" accepted-states)
+        input "10011"
+        expected {:input input :output "10100_" :end-state :q2 :verdict :accepted}
+        actual (tm/emulate-tm tm input)]
+    (is (= expected actual) (str "actual: " actual))))
 
 (deftest add-one-infront-of-input-test
   (testing "add one infront of input turingmachine")
